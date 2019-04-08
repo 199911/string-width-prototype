fe31e286a96efe966e5381ab1c53acc3dad59aa9
KU Lok Sun <sundaykuloksun@gmail.com>
2019-04-08 18:12:36 +0800

    Refactor testcase to make it reusable

--- test/index.js
+++ test/index.js
@@ -2,39 +2,43 @@ const { assert } = require('chai');
 
 const check0x7f = require('../check0x7f.js');
 
-describe('check0x7f', () => {
+const runTestcases = func => {
   it('should return 3 for 易樂𠒇', () => {
-    const actual = check0x7f("易樂𠒇");
+    const actual = func("易樂𠒇");
     assert.equal(actual, 3);
   });
 
   it('should return 1 for full-width space', () => {
-    const actual = check0x7f("　");
+    const actual = func("　");
     assert.equal(actual, 1);
   });
 
   it('should return 0.5 for half-width space', () => {
-    const actual = check0x7f(" ");
+    const actual = func(" ");
     assert.equal(actual, 0.5);
   });
 
   it('should return 1 for emoji 😀', () => {
-    const actual = check0x7f("😀");
+    const actual = func("😀");
     assert.equal(actual, 1);
   });
 
   it('should return 3 for 。。。', () => {
-    const actual = check0x7f("。。。");
+    const actual = func("。。。");
     assert.equal(actual, 3);
   });
 
   it('should reutrn 0.5 for Europian charactor like ÿ', () => {
-    const actual = check0x7f("ÿ");
+    const actual = func("ÿ");
     assert.equal(actual, 0.5);
   });
 
   it('should reutrn 0.5 for Symbol ｡ (0xff61)', () => {
-    const actual = check0x7f("｡");
+    const actual = func("｡");
     assert.equal(actual, 0.5);
   });
-})
+};
+
+describe('check0x7f', () => {
+  runTestcases(check0x7f);
+});
